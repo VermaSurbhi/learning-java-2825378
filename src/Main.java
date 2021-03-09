2c4fccd5f8d9f35c7ca4a881ae7a47a1b7b18488
Surbhi Verma <surbhi.verma@autotrader.co.uk>
2021-03-09 14:33:36 +0000

    Demonstrate function call with parameters

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import sun.lwawt.macosx.CSystemTray;
+
 public class Main {
 
-    public static void calculateTotalMealPrice() {
+    public static void calculateTotalMealPrice(double listedMealPrice,
+                                               double taxRate,
+                                               double tipRate) {
+        double tax = listedMealPrice * taxRate;
+        double tip = listedMealPrice * tipRate;
+        double result = listedMealPrice + tax + tip;
+
+        System.out.println("The total meal price is " + result);
     }
 
     public static void main(String[] args) {
+        calculateTotalMealPrice(15,.2,.08);
+        calculateTotalMealPrice(25,.18,.08);
     }
 
 }
