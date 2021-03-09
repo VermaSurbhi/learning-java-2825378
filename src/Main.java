16a6f3718bb9489ebc206700415973ff3fbaa410
Surbhi Verma <surbhi.verma@autotrader.co.uk>
2021-03-09 14:27:35 +0000

    Demonstrate function definition and function call

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void announceDeveloperTeaTime(){
+        System.out.println("Waiting for developer tea time...");
+        System.out.println("Type any random word and press enter");
 
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's time for Developers Tea! Let's have some snacks!");
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to your new job");
+        announceDeveloperTeaTime();
+
+        System.out.println("Write code");
+        System.out.println("Review code");
+
+        announceDeveloperTeaTime();
+
+        System.out.println("Congrats! You're promoted.");
     }
 
 }
