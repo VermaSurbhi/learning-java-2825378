345b5212cafe2506ae0172d388782196b4150c48
Surbhi Verma <surbhi.verma@autotrader.co.uk>
2021-03-09 17:02:27 +0000

    Salary Challenge

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
-
+    public static double calculateEmployeeGrossSalary(int numberOfHoursPerWeek,
+                                                      double amountPerHour,
+                                                      int numberOfVacationDays){
+        if (numberOfHoursPerWeek < 0 ){
+            return -1;
+        }
+        if (amountPerHour < 0) {
+            return -1;
+        }
+        double vacationDayIncome = (numberOfVacationDays * 8) * amountPerHour;
+        double grossSalary = (numberOfHoursPerWeek * amountPerHour * 52) - vacationDayIncome;
+        return grossSalary;
+    }
     public static void main(String[] args) {
+
+        double totalSalary = calculateEmployeeGrossSalary(40, 15, 8);
+        System.out.println("Employee's total salary is " + totalSalary);
+
+
         
     }
 
